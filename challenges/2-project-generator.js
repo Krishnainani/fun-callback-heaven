dfcd3f21f5e69ec1a69ec35253e11e4d38f8f59a
krishnainani <krishnainani4@gmail.com>
2022-07-13 12:20:05 +0100

    started section 2

--- challenges/2-project-generator.js
+++ challenges/2-project-generator.js
@@ -1,5 +1,24 @@
 const fs = require('fs');
 
-function projectGenerator(projectName, cb) {}
-
+function projectGenerator(projectName, cb) {
+    fs.mkdir(`${projectName}`, (err, directory) => {
+       // const fileNames = ["index.js", "index.test.js", "README.md", ".eslintrc.js",  ".gitignore"]
+       // let counter = 0;
+       // fileNames.forEach((file) => {
+        if(err){cb(err);}
+        else{
+       // fs.writeFile(`${projectName}/index.js`, (err, newFile) => {
+            //counter++;
+            cb(null, "directory created!")
+            // if(counter === fileNames.length){
+            // }
+        
+      //  })
+    }
+        })
+    
+}
+// projectGenerator("test_project", (err, string) => {
+//      console.log(string);
+// })
 module.exports = projectGenerator;
